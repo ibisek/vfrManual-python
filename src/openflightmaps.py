ef10493bfc3c546abeaa9a38093d4e5276934c67
ibisek <ibisek@protonmail.com>
2018-05-25 19:27:05 +0200

    can generate and update data from all over europe :) (still needs to be properly tested..)

--- src/openflightmaps.py
+++ src/openflightmaps.py
@@ -7,28 +7,24 @@ https://openflightmaps.org/live/lk-czech-republic/
 @author: ibisek
 '''
 
-import re
 import sys
 import json
 
 from bs4 import BeautifulSoup
+from html2json import removeDiacritic
 
-
-PATTERN_CMT1 = '<cmt>(.*?)[\[](.+?)\s([0-9.,]+)[\]]<[\/]cmt>'
-PATTERN_CMT2 = '<cmt>(.+?)\s([0-9.,]+)<\/cmt>'
-cmtPattern1 = re.compile(PATTERN_CMT1, re.IGNORECASE)
-cmtPattern2 = re.compile(PATTERN_CMT2, re.IGNORECASE)
-
-
-def getRunways(soup, codePrefix): 
+'''
+@return list of runways [direction, dimensions], e.g. ["14-32", "700x100"]
+'''
+def getRunways(soup, regionCode): 
     rwys = soup.find_all('rwy')
     
     runways = dict()
     
     for rwy in rwys:
         code=rwy.find('codeid').text
-        if not code.startswith(codePrefix): continue
-        directions = rwy.find('txtdesig').text
+        if not code.startswith(regionCode): continue
+        directions = rwy.find('txtdesig').text.replace(' ', '').replace('/', '-')
         length = rwy.find('vallen').text
         width = rwy.find('valwid').text
         
@@ -39,14 +35,14 @@ def getRunways(soup, codePrefix):
         
     return runways
 
-def getAirfields(soup, codePrefix):
+def getAirfields(soup, regionCode):
     records = soup.find_all('ahp')
     
     airfields = dict()
     
     for record in records:
         code=record.find('codeid').text
-        if not code.startswith(codePrefix): continue
+        if not code.startswith(regionCode): continue
         
         lat = record.find('geolat').text
         latLetter = lat[len(lat)-1]
@@ -68,24 +64,21 @@ def getAirfields(soup, codePrefix):
         # there are multiple (typically two) txt name fields - the shorter one contains name of the place:
         name = min([name.text for name in names], key=len)
         
-        if code not in airfields:
-            airfields[code] = list()
-         
-        airfields[code].append((name, lat, lon, (elevFt, elevM)))
+        airfields[code] = (name, lat, lon, (elevFt, elevM))
          
     return airfields
 
 '''
 @return dict of uni ID -> code; e.g. 30196 -> LKKA
 '''
-def _getUniIds(soup, codePrefix):
+def _getUniIds(soup, regionCode):
     records = soup.find_all('uni')  
     
     uniIds = dict()
     
     for record in records:
         code=record.find('codeid').text
-        if not code or not code.startswith(codePrefix): continue
+        if not code or not code.startswith(regionCode): continue
         
         uniId = record.uniuid['mid']
         
@@ -96,8 +89,8 @@ def _getUniIds(soup, codePrefix):
 '''
 @return dict (key = code) of lists of tuples (callSign, freq)
 '''
-def getFrequencies(soup, codePrefix):
-    uniIds = _getUniIds(soup, codePrefix)
+def getFrequencies(soup, regionCode):
+    uniIds = _getUniIds(soup, regionCode)
     
     records = soup.find_all('fqy')  
      
@@ -121,7 +114,7 @@ def getFrequencies(soup, codePrefix):
     return frequencies
 
 
-def doProcess(filename, outPath, codePrefix="LK"):
+def doProcess(filename, workingDir, regionCode='LK'):
 
     f = open(filename, 'r')
     text = f.read()
@@ -129,34 +122,78 @@ def doProcess(filename, outPath, codePrefix="LK"):
 
     soup = BeautifulSoup(text, 'html.parser')
     
-    runways = getRunways(soup, codePrefix)
+    runways = getRunways(soup, regionCode)
     print("runways:", runways)
     
-    airfields = getAirfields(soup, codePrefix)
+    airfields = getAirfields(soup, regionCode)
     print("airfields:", airfields)
     
-    frequencies = getFrequencies(soup, codePrefix)
+    frequencies = getFrequencies(soup, regionCode)
     print("frequencies:", frequencies)
     
+#     sys.exit(0)
     
+    for code in airfields.keys():
+        af = airfields[code]              # (name, lat, lon, (elevFt, elevM))
+        freq = frequencies[code]    # (callSign, freq)
+        rwys = runways[code]        # [(directions, dimensions)]
+        
+        #try to locate existing file or start new if not present
+        filename = "{}/{}.json".format(workingDir, code.lower())
+        
+        try:
+            f = open(filename, 'r')
+            jsonStr = f.read()
+            f.close()
+            j = json.loads(jsonStr)
+
+            # add fields which are missing:
+            if 'rwy' not in j:
+                j['rwy'] = rwys
+            
+            if 'elev' not in j:
+                j['elev'] = af[3]
+        
+        
+        except FileNotFoundError:
+            # create a completely new record + file:
+            j = dict()
+            j['code'] = code
+            j['name'] = af[0]
+            j['alias'] = removeDiacritic(af[0])
+            j['coords'] = (af[1], af[2]) 
+            j['elev'] = af[3]
+            
+            j['freq'] = [("{} {}".format(af[0], f[0]), f[1]) for f in freq]
+            j['rwy'] = rwys
+            
+        
+        # save the json:
+        s = json.dumps(j, separators=(',',':'))
+        f = open(filename, 'w')
+        f.write(s)
+        f.close()
+    
+
+TEST = True    
 if __name__ == '__main__':
 
-#     if TEST:
-#         filename = '/tmp/00/ulonly.xml'
-#         outPath = '/tmp/00/'
-#              
-#     else:
-#         if len(sys.argv) != 3:
-#             print("Usage: html2json <filename> <outPath>\n where filename is ulonly.xml")
-#             sys.exit(0)
-#      
-#         filename = sys.argv[1]
-#         outPath = sys.argv[2]
+    if TEST:
+        filename = '/home/ibisek/wqz/download/vfrManual/openflightmaps.org/aixm_lk.xml'
+        workingDir = '/tmp/00'
+        regionCode = 'LK'
+              
+    else:
+        if len(sys.argv) != 3:
+            print("Usage: openflightmaps.py <aixm-filename> <outPath> <regionCode>\n where\n  filename is from the embedded folder and\n  regionCode is LK/LZ/..")
+            sys.exit(0)
+      
+        filename = sys.argv[1]
+        workingDir = sys.argv[2]
+        regionCode = sys.argv[3]
 
-    filename = "/home/ibisek/wqz/download/vfrManual/openflightmaps.org/aixm_lk.xml"
-    outPath = "/tmp/00"
  
-    doProcess(filename, outPath)
+    doProcess(filename, workingDir, regionCode)
     
     print("KOHEU.")
     
