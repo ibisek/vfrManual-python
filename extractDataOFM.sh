c642b62a439a263b04313632aca2adaab0a732a9
ibisek <ibisek@protonmail.com>
2019-04-29 23:06:48 +0200

    +OFM

---
+++ extractDataOFM.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# for openflightmaps.org
+#
+
+srcDir='/tmp/00'
+workDir='/home/ibisek/wqz/prog/android/vfrManual/app/src/main/assets/json'
+
+
+echo "Processing LK.."
+python3 ./src/openflightmaps.py LK $srcDir/aixm_lk.xml $workDir
+echo "Processing LZ.."
+python3 ./src/openflightmaps.py LZ $srcDir/aixm_lz.xml $workDir
+echo "Processing EP.."
+python3 ./src/openflightmaps.py EP $srcDir/aixm_ep.xml $workDir
+
+
+echo "done."
