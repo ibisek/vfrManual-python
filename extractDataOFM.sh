edcb35ae98db8d6662ac1c8da24d4924f47e4f4b
ibisek <ibisek@protonmail.com>
2020-05-29 22:00:31 +0200

    process all available files

--- extractDataOFM.sh
+++ extractDataOFM.sh
@@ -8,16 +8,17 @@ workDir='/home/ibisek/wqz/prog/android/vfrManual/app/src/main/assets/json'
 
 source ./venv/bin/activate
 
-echo "Processing LK.."
-python3 ./src/openflightmaps.py LK $srcDir/ofmx_lk.ofmx $workDir
-echo "Processing LZ.."
-python3 ./src/openflightmaps.py LZ $srcDir/ofmx_lz.ofmx $workDir
-echo "Processing EP.."
-python3 ./src/openflightmaps.py EP $srcDir/ofmx_ep.ofmx $workDir
-echo "Processing ED.."
-python3 ./src/openflightmaps.py ED $srcDir/ofmx_ed.ofmx $workDir
-echo "Processing LO.."
-python3 ./src/openflightmaps.py LO $srcDir/ofmx_lo.ofmx $workDir
+files="$srcDir/*.ofmx"
+
+for fn in $files
+do
+    regionCode=`echo $fn|sed 's/.*_\(.\{2\}\).*/\1/' | tr a-z A-Z`
+
+    echo "Processing '$regionCode' -> '$fn'"
+    
+    python3 ./src/openflightmaps.py $regionCode $fn $workDir
+done
+
 
 deactivate
 
